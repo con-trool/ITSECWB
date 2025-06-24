939e1d17cbe382122f52614667309cebd713fcb3
con-trool <con_serrano@dlsu.edu.ph>
2025-06-24 23:48:57 +0800

    first commit

---
+++ database/models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    userID: Number,
+    username: String,
+    password: String,
+    name: String,
+    college: String,
+    program: String,
+    description: String,
+    image: String,
+    isTechnician: Boolean,
+    securityQuestion: String,
+    securityAnswer: String
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
